0d5aee13878d5ceaa941c8c8aa1c351f1c1df6f7
abernatskiy <abernatskiy@toha.li>
2023-06-29 01:05:43 +0900

    Adding a BSC processor
    	modified:   commands.json
    	new file:   src/main-bsc.ts
    	modified:   src/main-eth.ts
    	new file:   src/processor-bsc.ts

--- src/main-bsc.ts
+++ src/main-bsc.ts
@@ -1,18 +1,18 @@
 import {TypeormDatabase} from '@subsquid/typeorm-store'
 import {Transfer} from './model'
-import {processor, ETH_USDC_ADDRESS} from './processor-eth'
+import {processor, BSC_USDC_ADDRESS} from './processor-bsc'
 import * as erc20abi from './abi/erc20'
 
-processor.run(new TypeormDatabase({supportHotBlocks: false, stateSchema: 'eth_processor'}), async (ctx) => {
+processor.run(new TypeormDatabase({supportHotBlocks: false, stateSchema: 'bsc_processor'}), async (ctx) => {
     const transfers: Transfer[] = []
     for (let c of ctx.blocks) {
         for (let log of c.logs) {
-            if (log.address !== ETH_USDC_ADDRESS || log.topics[0] !== erc20abi.events.Transfer.topic) continue
+            if (log.address !== BSC_USDC_ADDRESS || log.topics[0] !== erc20abi.events.Transfer.topic) continue
             let {from, to, value} = erc20abi.events.Transfer.decode(log)
             transfers.push(
                 new Transfer({
                     id: log.id,
-                    network: 'eth',
+                    network: 'bsc',
                     block: c.header.height,
                     timestamp: new Date(c.header.timestamp),
                     from,
